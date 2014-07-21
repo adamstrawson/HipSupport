cadbab1a41abc4a31d0c00f6f1bc17b15e18422f
Adam Strawson <adam@adamstrawson.com>
2014-07-21 22:12:49 +0100

    Updated plugin docs

--- hipsupport.php
+++ hipsupport.php
@@ -6,8 +6,25 @@ Description: HipChat Support on your site!
 Version: 0.1
 Author: Adam Strawson 
 Author URI: http://www.adamstrawson.com
-License: GPL2 
+License: GPL v3
+
+HipSupport Plugin
+Copyright (C) 2014, Adam Strawson - adam@adamstrawson.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 /*
  * Require various classes, and the admin panel
@@ -106,5 +123,3 @@ function hipsupport_ajax(){
 add_action( 'wp_ajax_hipsupport', 'hipsupport_ajax' );
 add_action( 'wp_ajax_nopriv_hipsupport', 'hipsupport_ajax' );
 add_action( 'wp_footer', 'hipsupport_frontend' );
-
-//EOF
